768561422f525d61743e74f7bc85bd26fe702ce6
rakesh89220 <rakeshyadav7852@gmail.com>
2024-03-20 23:00:15 +0530

    jl

---
+++ src/pages/home/Home.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import "./style.scss";
+import HeroBanner from './heroBanner/HeroBanner';
+// import { Treding } from './trending/Treding';
+import Treding from './trending/Treding';
+import Popular from './popular/Popular';
+import TopRated from '../topRated/TopRated';
+
+const Home = () => {
+  return (
+    <div className='homePage'>
+        <HeroBanner/>
+        <Treding/>
+        <Popular/>
+        <TopRated/>
+    </div>
+  )
+}
+
+export default Home;
